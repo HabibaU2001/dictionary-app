3e8708b129173c3945b29406c2f9db6e9aa1f05b
HabibaU2001 <156705727+HabibaU2001@users.noreply.github.com>
2024-09-03 17:28:01 +0100

    Name changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Dictionary</h1>
+        <h1> Define it</h1>
       </header>
     </div>
   );
